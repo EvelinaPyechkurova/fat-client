15f3c6aa1d8889b027145041f244b0e8fe8a0e50
Evelina_Pyechkurova <evelina.pyechkurova@gmail.com>
2024-11-09 20:12:07 +0200

    Subject can be filtered by filtersidebar

--- src/components/Subject/Subjects.js
+++ src/components/Subject/Subjects.js
@@ -2,18 +2,46 @@ import SubjectList from './SubjectList';
 import useFetchData from '../../hooks/useFetchData';
 import { useNavigate } from 'react-router-dom';
 import FilterSidebar from '../FilterSidebar';
+import { useEffect, useState } from 'react';
+import {YERS_VALUES, TRIMESTER_TYPE_VALUES} from '../../utils/constants';
 
 const Subjects = () => {
-    const {data:subjects, isLoading, error} = useFetchData('http://localhost:3000/subjects');
     const navigate = useNavigate();
+
+    const [filterQuery, setFilterQuery] = useState("");
+    const [url, setUrl] = useState('http://localhost:3000/subjects');
+    
+    const {data:subjects, isLoading, error} = useFetchData(url);
 
     const handleCreateSubject = () => {
         navigate("/subjects/create");
     }
+
+    const {data:teachers} = useFetchData('http://localhost:3000/teachers');
+    const teacherOptions = teachers ? teachers.map(teacher => ({
+        value: teacher._id,
+        label: `${teacher.surname} ${teacher.name}`
+    })) : [];
+
+    const fields = [
+        {name: "name", label: "subjects name"},
+        {name: "year", label: "subjects year", options: YERS_VALUES},
+        {name: "trimester", label: "trimester year", options: TRIMESTER_TYPE_VALUES},
+        {name: "teacher", label: "subjects read by teacher", options: teacherOptions}
+    ];
+
+    const handleSearch = (query) => {
+        setFilterQuery(query);
+    }
+
+    useEffect(() => {
+        const newUrl = `http://localhost:3000/subjects${filterQuery ? `?${filterQuery}` : ''}`;
+        setUrl(newUrl);
+    }, [filterQuery]);
     
     return(
         <div className="subjects">
-            <FilterSidebar/>
+            <FilterSidebar fields={fields} onSearch={handleSearch}/>
             <button className='create-button' onClick={handleCreateSubject}>add new subject</button>
             {error && <div>{error}</div>}
             {isLoading && <div>Loading...</div>}
